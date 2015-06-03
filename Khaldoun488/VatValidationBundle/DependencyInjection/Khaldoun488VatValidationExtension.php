fd6ca82c67cf10b71c522e118b6910a2b60c2c7d
khaldoun <khaldoun@iron-mail.net>
2015-06-03 19:50:30 +0200

    create the service container

---
+++ Khaldoun488/VatValidationBundle/DependencyInjection/Khaldoun488VatValidationExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Khaldoun488\VatValidationBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+/**
+ * Class Khaldoun488VatValidationExtension
+ */
+class Khaldoun488VatValidationExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+    }
+}
